d020359618cec137348ca67f5ba156235c4a4728
Autrikektor <alexey@aristov.biz>
2024-02-18 19:57:33 +0400

    v2

--- start.bat
+++ start.bat
@@ -1,11 +1,17 @@
-@echo off
+@echo off 
+
 ECHO Starting Bot
 :loop
 
 git checkout -f .
 git pull
 cmd.exe /c npm i --force --no-audit
+
+timeout /t 5
+
 node xbot-worker.js --env-file="settings.example"
+
+timeout /t 5
 
 goto loop
 
